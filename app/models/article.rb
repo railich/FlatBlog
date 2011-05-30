b9d05867d5da944bdb0a46c889d3864c471fc652
Rail <railich@bk.ru>
2011-05-30 12:08:03 +0400

    validates and relationships for article and comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ActiveRecord::Base
+  validates :title, :body, :presence => true
+  validates :title, :uniqueness => true
+  
+  has_many :comments, :dependent => :destroy
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ActiveRecord::Base
+  belongs_to :article
+  
+  validates :name, :body, :presence => true
 end
