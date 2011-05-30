e9f2f684fd8f556ce731f7cc564e11b39ada1ec1
Rail <railich@bk.ru>
2011-05-30 11:07:07 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlatBlog::Application.config.secret_token = '2eea42da371c5568fffcc4ca2a8034e837dd28c1ba9e9273cbf97776d563973020d11b96f480fa85d77a4a7273cc22872773375fd529d80f70d0793ad3e05467'
