c85e72d1e43d7322992f5a4f950c9f05f9a9b914
Rail <railich@bk.ru>
2011-05-30 11:27:15 +0400

    add Category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Category.create!([{:name =>'News'}, {:name =>'Sport'}, {:name =>'Humor'},{:name =>'World'}])
